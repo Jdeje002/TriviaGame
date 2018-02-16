cfdb5eb6e46989f0910d19243584d77e54843198
John De jesus <John@Johns-iMac.local>
2018-02-16 15:02:15 -0800

    added comments

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,8 +1,32 @@
 $(document).ready(function () {
+    
+    
     var rightAnswers = 0
     var wrongAnswers = 0
+    
     function initGame(){
         
+
+        // timer
+
+        // click button to start timer 
+
+        // show quiz
+
+        // keep track of right answers
+
+        // keep track of wrong answers
+
+        // desplay correct answers
+
+
     }
+  
+  
+  
+  
+  
     initGame()
+
 }
+
